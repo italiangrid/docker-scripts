b0823596f34d1bd7619ce134b41ea2677c871678
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2014-10-23 18:51:05 +0200

    Moved setup in setup.sh
    - Keep cert in repo instead of fetching it from dropbox

---
+++ voms-deployment-test/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -ex
+
+WGET="wget --no-check-certificate"
+DEPLOYMENT_TEST_SCRIPT=${DEPLOYMENT_TEST_SCRIPT:-https://raw.github.com/italiangrid/voms-deployment-test/master/voms-deployment-test.sh}
+
+## Setup host certificate
+mkdir -p /etc/grid-security
+cp /voms_server.cert.pem /etc/grid-security/hostcert.pem
+cp /voms_server.key.pem /etc/grid-security/hostkey.pem
+chmod 400 /voms_server.key.pem /etc/grid-security/hostkey.pem
+
+## Run puppet
+git clone git://github.com/cnaf/ci-puppet-modules.git /ci-puppet-modules
+puppet apply --modulepath=/modules:/etc/puppet/modules/ /manifest.pp
+
+# Fetch deployment test script
+wget --no-check-certificate ${DEPLOYMENT_TEST_SCRIPT} \
+  -O /voms-deployment-test.sh
+
+chmod +x /voms-deployment-test.sh
+
+## Clean things up
+yum clean all
