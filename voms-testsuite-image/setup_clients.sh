84aa946a28522e9d528b6892649060a408705f93
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2014-10-23 17:59:14 +0200

    Fixes to voms-testsuite image

--- voms-testsuite-image/setup_clients.sh
+++ voms-testsuite-image/setup_clients.sh
@@ -1,12 +1,50 @@
 #!/bin/bash
 set -x
 
-VO_HOST=${VO_HOST:-vgrid02.cnaf.infn.it}
-VO_PORT=${VO_PORT:-15000}
-VO=${VO:-test.vo}
-VO_ISSUER=${VO_ISSUER:-/C=IT/O=INFN/OU=Host/L=CNAF/CN=vgrid02.cnaf.infn.it}
-VO_ISSUER_CA=${VO_ISSUER_CA:-/C=IT/O=INFN/CN=INFN CA}
+## The testsuite repo
 TESTSUITE=${TESTSUITE:-git://github.com/italiangrid/voms-testsuite.git}
+
+## VO 1 configuration
+VO1_HOST=${VO1_HOST:-vgrid02.cnaf.infn.it}
+VO1_PORT=${VO1_PORT:-15000}
+VO1=${VO1:-test.vo}
+VO1_ISSUER=${VO1_ISSUER:-/C=IT/O=INFN/OU=Host/L=CNAF/CN=vgrid02.cnaf.infn.it}
+
+## VO 2 configuration
+VO2_HOST=${VO2_HOST:-vgrid02.cnaf.infn.it}
+VO2_PORT=${VO2_PORT:-15001}
+VO2=${VO2:-test.vo.2}
+VO2_ISSUER=${VO2_ISSUER:-/C=IT/O=INFN/OU=Host/L=CNAF/CN=vgrid02.cnaf.infn.it}
+
+## Creates a VOMSES file
+make_vomses() {
+  local vo_name=$1
+  local vo_host=$2
+  local vo_port=$3
+  local vo_issuer=$4
+
+  rm -rf /etc/vomses/${vo_name}
+  cat << EOF > /etc/vomses/${vo_name}
+  "${vo_name}" "${vo_host}" "${vo_port}" "${vo_issuer}" "${vo_name}"
+EOF
+  cat /etc/vomses/${vo_name}
+}
+
+## Creates an LSC file
+make_lsc(){
+  local vo_name=$1
+  local vo_host=$2
+  local vo_port=$3
+
+  local make_lsc_cmd="openssl s_client -connect ${vo_host}:${vo_port} | \
+    openssl x509 -noout -subject -issuer | \
+    sed -e 's/subject= //g' -e 's/issuer= //g'"
+
+  mkdir -p /etc/grid-security/vomsdir/${vo_name}
+  eval ${make_lsc_cmd} > /etc/grid-security/vomsdir/${vo_name}/${vo_host}.lsc
+  cat /etc/grid-security/vomsdir/${vo_name}/${vo_host}.lsc
+}
+
 
 # check and install the extra repo for VOMS clients if provided by user
 if [ -z $VOMSREPO ]; then
@@ -21,38 +59,33 @@ fi
 yum install -y voms-clients3
 yum install -y myproxy
 
-## Create VOMSES file for VO
-rm -rf /etc/vomses/${VO}*
-cat << EOF > /etc/vomses/${VO}-${VO_HOST}
-"${VO}" "${VO_HOST}" "${VO_PORT}" "${VO_ISSUER}" "${VO}"
-EOF
+## Setup vomses file for the two test VOs
+make_vomses ${VO1} ${VO1_HOST} ${VO1_PORT} ${VO1_ISSUER}
+make_vomses ${VO2} ${VO2_HOST} ${VO2_PORT} ${VO2_ISSUER}
 
-## Print out VOMSES file content
-cat /etc/vomses/${VO}-${VO_HOST}
-
-make_lsc_cmd="openssl s_client -connect ${VO_HOST}:${VO_PORT} | openssl x509 -noout -subject -issuer | sed -e 's/subject= //g' -e 's/issuer= //g'"
-
-## Create LSC file for VO
-mkdir -p /etc/grid-security/vomsdir/${VO}
-eval ${make_lsc_cmd} > /etc/grid-security/vomsdir/${VO}/${VO_HOST}.lsc
-
-## Print out lSC file content
-cat /etc/grid-security/vomsdir/${VO}/${VO_HOST}.lsc
+## Setup LSC file for the two test VOs
+make_lsc ${VO1} ${VO1_HOST} ${VO1_PORT}
+make_lsc ${VO2} ${VO2_HOST} ${VO2_PORT}
 
 cat << EOF > /home/voms/run-testsuite.sh
 #!/bin/bash 
 set -ex
 git clone $TESTSUITE
 pushd ./voms-testsuite
-pybot --variable vo1_host:$VO_HOST \
-  --variable vo1:$VO \
-  --variable vo1_issuer:$VO_ISSUER \
-  --pythonpath lib -d reports \
+pybot \
+  --variable vo1:$VO1 \
+  --variable vo1_host:$VO1_HOST \
+  --variable vo1_issuer:$VO1_ISSUER \
+  --variable vo2:$VO2 \
+  --variable vo2_host:$VO2_HOST \
+  --variable vo2_issuer:$VO2_ISSUER \
+  --pythonpath lib \
+  -d reports \
   tests/clients
 EOF
 
 chmod +x /home/voms/run-testsuite.sh
 chown voms:voms /home/voms/run-testsuite.sh
 
-# install and execute the VOMS testsuite as user "voms"
+# install and execute the VOMS testsuite as user voms
 su - voms -c /home/voms/run-testsuite.sh
