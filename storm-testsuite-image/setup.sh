fd2b73a6ad575a9e3b8f7dc280014b7f3c268541
dandreotti <daniele.andreotti@cnaf.infn.it>
2014-10-27 15:38:29 +0100

    Added check of the exit code of puppet apply

--- storm-testsuite-image/setup.sh
+++ storm-testsuite-image/setup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-#install the list of puppet modules after downloading from github
+# install the list of puppet modules after downloading from github
 git clone git://github.com/cnaf/ci-puppet-modules.git /ci-puppet-modules
 
-#install all puppet modules required by the StoRM testsuite
+# install all puppet modules required by the StoRM testsuite. 
+# the "--detailed-exitcodes" flag returns explicit exit status:
+# exit code '2' means there were changes
+# exit code '4' means there were failures during the transaction
+# exit code '6' means there were both changes and failures
 puppet apply --modulepath=/ci-puppet-modules/modules:/etc/puppet/modules/ --detailed-exitcodes /manifest.pp
 
-# check if errors occurred when applying puppet
-if [ $? -eq 2 ]; then
+# check if errors occurred after puppet apply:
+if [[ ( $? -eq 4 ) || ( $? -eq 6 ) ]]; then
   exit 1
 fi
 
