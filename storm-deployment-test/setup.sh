3aca7df7feb97cb17f5439f385dd433467ed1f72
dandreotti <daniele.andreotti@cnaf.infn.it>
2014-10-27 16:32:01 +0100

    Added check on the exit code of puppet apply

--- storm-deployment-test/setup.sh
+++ storm-deployment-test/setup.sh
@@ -3,8 +3,17 @@
 #install the list of puppet modules after downloading from github
 git clone git://github.com/cnaf/ci-puppet-modules.git /ci-puppet-modules
 
-#install all puppet modules required by the StoRM testsuite
-puppet apply --modulepath=/ci-puppet-modules/modules:/etc/puppet/modules/ /manifest.pp
+# install all puppet modules required by the StoRM testsuite. 
+# the "--detailed-exitcodes" flag returns explicit exit status:
+# exit code '2' means there were changes
+# exit code '4' means there were failures during the transaction
+# exit code '6' means there were both changes and failures
+puppet apply --modulepath=/ci-puppet-modules/modules:/etc/puppet/modules/ --detailed-exitcodes /manifest.pp
+
+# check if errors occurred after puppet apply:
+if [[ ( $? -eq 4 ) || ( $? -eq 6 ) ]]; then
+  exit 1
+fi
 
 # install acl and extended attributes support
 yum install -y attr acl
