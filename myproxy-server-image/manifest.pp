ea322150655d646b10cdcb3ffa4d45b71dd47b1e
dandreotti <daniele.andreotti@cnaf.infn.it>
2014-11-05 16:50:55 +0100

    Skeleton scripts for myproxy server image

---
+++ myproxy-server-image/manifest.pp
@@ -0,0 +1,4 @@
+include puppet-test-ca
+include puppet-infn-ca
+include puppet-centos-utils::fix-stap-groups
+include puppet-centos-utils::yum-conf
---
+++ myproxy-server-image/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#install the list of puppet modules after downloading from github
+git clone git://github.com/cnaf/ci-puppet-modules.git /ci-puppet-modules
+
+# install all puppet modules required  
+# the "--detailed-exitcodes" flag returns explicit exit status:
+# exit code '2' means there were changes
+# exit code '4' means there were failures during the transaction
+# exit code '6' means there were both changes and failures
+puppet apply --modulepath=/ci-puppet-modules/modules:/etc/puppet/modules/ --detailed-exitcodes /manifest.pp
+
+# check if errors occurred after puppet apply:
+if [[ ( $? -eq 4 ) || ( $? -eq 6 ) ]]; then
+  exit 1
+fi
+
+# install acl and extended attributes support
+yum install -y fetch-crl
+
+# run fetch-crl
+fetch-crl
+
+# check if errors occurred after fetch-crl execution
+if [ $? != 0 ]; then
+  exit 1
+fi
+
