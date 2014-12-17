44ff5bcbe2aca7496d18ee572c21cde8bf7c02b0
dandreotti <daniele.andreotti@cnaf.infn.it>
2014-12-17 14:18:49 +0100

    Removed tag and push to the docker registry

--- base/build-image.sh
+++ base/build-image.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 docker build --force-rm=true --no-cache -t italiangrid:base .
-docker tag -f italiangrid:base cloud-vm128.cloud.cnaf.infn.it/italiangrid:base
-docker push cloud-vm128.cloud.cnaf.infn.it/italiangrid:base
