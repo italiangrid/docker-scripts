a79eb24c7ac41344432b992955c2943e81994fdf
dandreotti <daniele.andreotti@cnaf.infn.it>
2014-12-17 13:49:36 +0100

    Removed login to registry

--- base/build-image.sh
+++ base/build-image.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 docker build --force-rm=true --no-cache -t italiangrid:base .
-docker login --password=${REGISTRY_PASSWORD} --username="jenkins" --email=" " https://cloud-vm128.cloud.cnaf.infn.it
-docker tag italiangrid:base cloud-vm128.cloud.cnaf.infn.it/italiangrid:base
+docker tag -f italiangrid:base cloud-vm128.cloud.cnaf.infn.it/italiangrid:base
 docker push cloud-vm128.cloud.cnaf.infn.it/italiangrid:base
