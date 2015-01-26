222f0c00c04812225b23a6d1a603ebac4c3cfda9
dandreotti <daniele.andreotti@cnaf.infn.it>
2015-01-26 10:06:18 +0100

    Increased retries value to avoid timeout

--- storm-testsuite-image/setup_testsuite.sh
+++ storm-testsuite-image/setup_testsuite.sh
@@ -5,7 +5,7 @@ STORM_BE_HOST="${STORM_BE_HOST:-docker-storm.cnaf.infn.it}"
 
 echo 'export X509_USER_PROXY="/tmp/x509up_u$(id -u)"'>/etc/profile.d/x509_user_proxy.sh
 
-MAX_RETRIES=400
+MAX_RETRIES=600
 
 attempts=1
 
