2d7d5b4179b3258ea88b3a4b5dc99eb3511af20f
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2015-05-08 16:48:01 +0200

    Add support for mock HR db in container

--- voms-admin-server/setup/run.sh
+++ voms-admin-server/setup/run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -ex
 
-groupadd -r voms
-useradd voms -g voms
+chown voms:voms /home/voms
+usermod -m -d /home/voms voms
+
+cp /tnsnames.ora /home/voms
+chown voms:voms /home/voms/tnsnames.ora
 
 ls -l /code
+
+old_jars=$(ls /var/lib/voms-admin/lib/ | grep -v ojdbc6.jar)
+rm -f $old_jars
 
 tar -C / -xvzf /code/voms-admin-server/target/voms-admin-server.tar.gz
 
@@ -65,6 +71,16 @@ cp /logback.xml /etc/voms-admin/voms-admin-server.logback
 cp /logback.xml /etc/voms-admin/test/logback.xml
 chown voms:voms /etc/voms-admin/voms-admin-server.logback /etc/voms-admin/test/logback.xml
 
+# Setup orgdb.properties, if the orgdb volume is mounted
+if [ -e "/orgdb/orgdb.properties" ]; then
+  cp /orgdb/orgdb.properties /etc/voms-admin/test/orgdb.properties
+  chown voms:voms /etc/voms-admin/test/orgdb.properties
+
+  # Just a newline
+  echo >> /etc/voms-admin/test/service.properties
+  cat /orgdb.template >> /etc/voms-admin/test/service.properties
+fi
+
 # Deploy test vo
 touch '/var/lib/voms-admin/vo.d/test'
 
@@ -75,7 +91,7 @@ JAVA_LOG_LEVEL=${JAVA_LOG_LEVEL:-ERROR}
 VOMS_JAVA_OPTS="-Dvoms.dev=true -DVOMS_LOG_LEVEL=${VOMS_LOG_LEVEL} -DJAVA_LOG_LEVEL=${JAVA_LOG_LEVEL} $VOMS_JAVA_OPTS"
 
 if [ -n "$ENABLE_JREBEL" ]; then
-    VOMS_JAVA_OPTS="-javaagent:/jrebel/jrebel.jar -Drebel.stats=false -Drebel.usage_reporting=false $VOMS_JAVA_OPTS"
+    VOMS_JAVA_OPTS="-javaagent:/jrebel/jrebel.jar -Drebel.stats=false -Drebel.usage_reporting=false -Drebel.struts2_plugin=true -Drebel.tiles2_plugin=true $VOMS_JAVA_OPTS"
 fi
 
 if [ -z "$VOMS_DEBUG_PORT" ]; then
@@ -90,8 +106,17 @@ if [ ! -z "$VOMS_DEBUG" ]; then
   VOMS_JAVA_OPTS="-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=$VOMS_DEBUG_PORT,suspend=$VOMS_DEBUG_SUSPEND $VOMS_JAVA_OPTS"
 fi
 
+if [ -n "$ENABLE_JMX" ]; then
+  VOMS_JAVA_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=6002 -Dcom.sun.management.jmxremote.rmi.port=6002 -Djava.rmi.server.hostname=dev.local.io -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false $VOMS_JAVA_OPTS"
+fi
+
+ORACLE_LIBRARY_PATH=${ORACLE_LIBRARY_PATH:-/usr/lib64/oracle/11.2.0.3.0/client/lib64}
+TNS_ADMIN=${TNS_ADMIN:-/home/voms}
+
+ORACLE_ENV="LD_LIBRARY_PATH=$ORACLE_LIBRARY_PATH TNS_ADMIN=$TNS_ADMIN"
+
 ## Add test0 admin
 voms-db-util add-admin --vo test --cert /usr/share/igi-test-ca/test0.cert.pem || echo "Error creating test0 admin. Does it already exist?"
 
 # Start service
-su voms -s /bin/bash -c "java $VOMS_JAVA_OPTS -jar /usr/share/java/voms-container.jar $VOMS_ARGS"
+su voms -s /bin/bash -c "$ORACLE_ENV java $VOMS_JAVA_OPTS -jar /usr/share/java/voms-container.jar $VOMS_ARGS"
