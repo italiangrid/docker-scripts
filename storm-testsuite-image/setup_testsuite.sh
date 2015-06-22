1bef1132f46b72a61cf9bed66a24fac0e1b61515
enricovianello <maverick17586@gmail.com>
2015-06-22 15:22:03 +0200

    Added parameters to set the exclude tags and which tests run

--- storm-testsuite-image/setup_testsuite.sh
+++ storm-testsuite-image/setup_testsuite.sh
@@ -3,6 +3,8 @@
 TESTSUITE="${TESTSUITE:-git://github.com/italiangrid/storm-testsuite.git}"
 STORM_BE_HOST="${STORM_BE_HOST:-docker-storm.cnaf.infn.it}"
 TESTSUITE_BRANCH="${TESTSUITE_BRANCH:-develop}"
+TAGS_TO_EXCLUDE="${TAGS_TO_EXCLUDE:-to-be-fixed}"
+TESTSUITE_TESTS="${TESTSUITE_TESTS:-tests}"
 
 echo 'export X509_USER_PROXY="/tmp/x509up_u$(id -u)"'>/etc/profile.d/x509_user_proxy.sh
 
@@ -27,7 +29,13 @@ if [ $attempts -gt $MAX_RETRIES ]; then
     exit 1
 fi
 
+tags=(`echo $TAGS_TO_EXCLUDE | tr " " "\n"`)
+exclude_str=""
+for tag in "${tags[@]}"
+do
+  exclude_str="$exclude_str --exclude $tag"
+done
 
 # install and execute the StoRM testsuite (develop version) as user "tester"
-exec su - tester sh -c "git clone $TESTSUITE; cd /home/tester/storm-testsuite; git checkout $TESTSUITE_BRANCH; pybot --pythonpath lib --variable backEndHost:$STORM_BE_HOST --exclude to-be-fixed -d reports tests"
+exec su - tester sh -c "git clone $TESTSUITE; cd /home/tester/storm-testsuite; git checkout $TESTSUITE_BRANCH; pybot --pythonpath lib --variable backEndHost:$STORM_BE_HOST $exclude_str -d reports $TESTSUITE_TESTS"
 
