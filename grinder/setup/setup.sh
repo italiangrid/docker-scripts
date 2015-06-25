483a281605a83f81bf4d7697cf07a75a9cac4b81
enricovianello <maverick17586@gmail.com>
2015-06-25 16:47:24 +0200

    First version of grinder that works

--- grinder/setup/setup.sh
+++ grinder/setup/setup.sh
@@ -15,11 +15,14 @@ cp $CERT_DIR/$USER.cert.pem /home/tester/.globus/usercert.pem
 chmod 644 /home/tester/.globus/usercert.pem
 cp $CERT_DIR/$USER.key.pem /home/tester/.globus/userkey.pem
 chmod 400 /home/tester/.globus/userkey.pem
+chown tester:tester /home/tester/.globus/*.pem
 
 ls -latr /home/tester/.globus
 
 echo 'export X509_USER_PROXY="/tmp/x509up_u$(id -u)"'>/etc/profile.d/x509_user_proxy.sh
-
 cat /etc/profile.d/x509_user_proxy.sh
+
+echo 'export GRINDER_HOME="/opt/grinder-3.11"'>/etc/profile.d/grinder.sh
+cat /etc/profile.d/grinder.sh
 
 chmod +x /setup_testsuite.sh
--- grinder/setup/setup_testsuite.sh
+++ grinder/setup/setup_testsuite.sh
@@ -3,7 +3,9 @@
 TESTSUITE="${TESTSUITE:-git://github.com/italiangrid/grinder-load-testsuite.git}"
 TESTSUITE_BRANCH="${TESTSUITE_BRANCH:-master}"
 
-STORM_BE_HOST="${STORM_BE_HOST:-docker-storm.cnaf.infn.it}"
+STORM_BE_HOSTNAME="${STORM_BE_HOSTNAME:-docker-storm.cnaf.infn.it}"
+STORM_FE_ENDPOINT_LIST="${STORM_FE_ENDPOINT_LIST:-${STORM_BE_HOSTNAME}:8444}"
+STORM_DAV_ENDPOINT_LIST="${STORM_DAV_ENDPOINT_LIST:-${STORM_BE_HOSTNAME}:8443}"
 
 TESTSUITE_TEST_NAME="${TESTSUITE_TEST_NAME:-ptg-sync}"
 TESTSUITE_PROCESSES="${TESTSUITE_PROCESSES:-4}"
@@ -19,7 +21,7 @@ MAX_RETRIES=600
 
 attempts=1
 
-CMD="nc -z ${STORM_BE_HOST} 8444"
+CMD="nc -z ${STORM_BE_HOSTNAME} 8444"
 
 echo "Waiting for StoRM services... "
 $CMD
@@ -44,13 +46,13 @@ cmd="git clone $TESTSUITE; \
 	export GRINDER_USE_CONSOLE=$TESTSUITE_USE_CONSOLE; \
 	export GRINDER_CONSOLE_HOST=$TESTSUITE_CONSOLE_HOST; \
 	export GRINDER_RUNS=$TESTSUITE_RUNS; \
-	TEST="./storm/$TESTSUITE_TEST_NAME/test.properties"; \
+	export STORM_BE_HOSTNAME=$STORM_BE_HOSTNAME; \
+	export STORM_FE_ENDPOINT_LIST=$STORM_FE_ENDPOINT_LIST; \
+	export STORM_DAV_ENDPOINT_LIST=$STORM_DAV_ENDPOINT_LIST; \
 	rm -f log/*; \
-	./bin/runAgent.sh $TEST;"
+	./bin/runAgent.sh ./storm/$TESTSUITE_TEST_NAME/test.properties;"
 
 echo "$cmd"
 
 # install and execute the StoRM load testsuite as user "tester"
-exec su - tester sh -c "echo $PASSWORD|voms-proxy-init -pwstdin --voms $VO"
-exec su - tester sh -c "$cmd"
-
+exec su - tester sh -c "echo $PASSWORD|voms-proxy-init -pwstdin --voms $VO; $cmd"
