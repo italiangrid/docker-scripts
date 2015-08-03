36078ab0e949f637caa57718d819edf12e5bb764
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2015-08-03 14:12:29 +0200

    Robot framework 2.9 compatibility fix
    Execution directory is no longer added to module search
    path automatically.
    
    See:
    https://github.com/robotframework/robotframework/issues/2019

--- storm-testsuite-image/setup_testsuite.sh
+++ storm-testsuite-image/setup_testsuite.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 TESTSUITE="${TESTSUITE:-git://github.com/italiangrid/storm-testsuite.git}"
 STORM_BE_HOST="${STORM_BE_HOST:-docker-storm.cnaf.infn.it}"
@@ -29,9 +30,7 @@ if [ $attempts -gt $MAX_RETRIES ]; then
     exit 1
 fi
 
-pybot_cmd="pybot --pythonpath lib --variable backEndHost:$STORM_BE_HOST --exclude $TESTSUITE_EXCLUDE -d reports -s $TESTSUITE_SUITE tests"
-echo "pybot command = $pybot_cmd"
+pybot_cmd="pybot --pythonpath .:lib --variable backEndHost:$STORM_BE_HOST --exclude $TESTSUITE_EXCLUDE -d reports -s $TESTSUITE_SUITE tests"
 
 # install and execute the StoRM testsuite (develop version) as user "tester"
 exec su - tester sh -c "git clone $TESTSUITE; cd /home/tester/storm-testsuite; git checkout $TESTSUITE_BRANCH; $pybot_cmd"
-
