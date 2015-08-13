b3c35a5c17432f9a0a87419b5ce28d7271351cdd
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2015-08-13 11:20:52 +0200

    centos5 build image

--- build/build-images.sh
+++ build/build-images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DOCKER_REGISTRY_HOST=${DOCKER_REGISTRY_HOST:-cloud-vm128.cloud.cnaf.infn.it}
-IMAGES="centos6 centos7 argus-pap-centos7 argus-pap-rpm-centos7 \
+IMAGES="centos5 centos6 centos7 argus-pap-centos7 argus-pap-rpm-centos7 \
   repo-centos7 argus-pdp-pep-common-rpm-centos7 argus-pdp-rpm-centos7 argus-pep-api-c-rpm-centos7 \
   argus-pep-cli-rpm-centos7 argus-pep-common-rpm-centos7 argus-pep-server-rpm-centos7 mvnrepo"
 
---
+++ build/centos5/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -ex
+
+BUILD_USER=${BUILD_USER:-build}
+BUILD_USER_UID=${BUILD_USER_UID:-1234}
+BUILD_USER_HOME=${BUILD_USER_HOME:-/home/${BUILD_USER}}
+
+# Use only GARR and CERN mirrors
+echo "include_only=.garr.it,.cern.ch" >> /etc/yum/pluginconf.d/fastestmirror.conf
+
+yum clean all
+yum install -y hostname epel-release
+yum -y update
+yum -y install which make createrepo wget rpm-build git tar java-1.7.0-openjdk-devel apache-maven
+
+java -version
+javac -version
+
+source /etc/profile.d/apache-maven.sh
+mvn --version
+
+adduser --uid ${BUILD_USER_UID} ${BUILD_USER}
+mkdir ${BUILD_USER_HOME}/.m2
+cp /settings.xml ${BUILD_USER_HOME}/.m2
+mkdir /m2-repository
+
+chown -R ${BUILD_USER}:${BUILD_USER} ${BUILD_USER_HOME} /m2-repository
+yum clean all
