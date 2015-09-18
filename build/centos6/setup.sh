41be24c66cedda7f542ffc5973a4df69c991d49e
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2015-09-18 16:44:55 +0200

    RPM dist is set consistently with centos7

--- build/centos6/setup.sh
+++ build/centos6/setup.sh
@@ -13,7 +13,10 @@ yum install -y hostname epel-release
 yum -y update
 yum -y install which make createrepo \
   wget rpm-build git tar java-1.8.0-openjdk-devel apache-maven \
-  redhat-rpm-config buildsys-macros
+  redhat-rpm-config
+
+# Align it with centos7 naming
+sed -i -e "s#^%dist .el6#%dist .el6.centos#" /etc/rpm/macros.dist
 
 # Setup JAVA 8 as default for maven
 echo 'JAVA_HOME=$JVM_ROOT/java-1.8.0' >> /etc/java/java.conf
