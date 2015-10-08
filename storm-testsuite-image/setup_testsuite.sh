8f33b98954e746aa55320a1bc46685c669e7f65f
enricovianello <maverick17586@gmail.com>
2015-10-08 11:52:52 +0200

    Fixed EXCLUDE and added httplibrary
    TESTSUITE_EXCLUDE default value conflict with suite values which include no tests with to-be-fixed tag
    robotframework-httplibrary is used by latest tests added to testsuite

--- storm-testsuite-image/setup_testsuite.sh
+++ storm-testsuite-image/setup_testsuite.sh
@@ -5,7 +5,6 @@ TESTSUITE="${TESTSUITE:-git://github.com/italiangrid/storm-testsuite.git}"
 STORM_BE_HOST="${STORM_BE_HOST:-docker-storm.cnaf.infn.it}"
 STORM_BE_SYNC_PORT="${STORM_BE_SYNC_PORT:-8444}"
 TESTSUITE_BRANCH="${TESTSUITE_BRANCH:-develop}"
-TESTSUITE_EXCLUDE="${TESTSUITE_EXCLUDE:-to-be-fixed}"
 TESTSUITE_SUITE="${TESTSUITE_SUITE:-tests}"
 
 echo 'export X509_USER_PROXY="/tmp/x509up_u$(id -u)"'>/etc/profile.d/x509_user_proxy.sh
@@ -34,7 +33,13 @@ if [ $attempts -gt $MAX_RETRIES ]; then
     exit 1
 fi
 
-pybot_cmd="pybot --pythonpath .:lib --variable backEndHost:$STORM_BE_HOST --exclude $TESTSUITE_EXCLUDE -d reports -s $TESTSUITE_SUITE tests"
+if [ -z "$TESTSUITE_EXCLUDE" ]; then
+  EXCLUDE=""
+else
+  EXCLUDE="--exclude $TESTSUITE_EXCLUDE" 
+fi
+
+pybot_cmd="pybot --pythonpath .:lib --variable backEndHost:$STORM_BE_HOST $EXCLUDE -d reports -s $TESTSUITE_SUITE tests"
 
 # install and execute the StoRM testsuite (develop version) as user "tester"
 exec su - tester sh -c "git clone $TESTSUITE; cd /home/tester/storm-testsuite; git checkout $TESTSUITE_BRANCH; $pybot_cmd"
