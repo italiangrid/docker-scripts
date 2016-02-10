bfee54e7d15a8ba5ee46f7ebba7083d6492f0ad5
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2016-02-10 07:26:50 +0100

    upgrade docker-client to 1.9.1 in jenkins-slave

--- jenkins-slave/setup.sh
+++ jenkins-slave/setup.sh
@@ -2,24 +2,24 @@
 set -ex
 
 DOCKER_GID="233"
-
+apt-get clean all
 apt-get update -qq
 apt-get install -qqy apt-transport-https
 
 ## Latest Docker repo (1.8)
-# echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
-# apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
 ## Docker repo (1.6.2)
-echo "deb https://get.docker.io/ubuntu docker main" > /etc/apt/sources.list.d/docker.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+#echo "deb https://get.docker.io/ubuntu docker main" > /etc/apt/sources.list.d/docker.list
+#apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 
 ## EugridPMA repo
 echo "deb http://repository.egi.eu/sw/production/cas/1/current egi-igtf core" > /etc/apt/sources.list.d/egi-cas.list
 wget -q -O - https://dist.eugridpma.info/distribution/igtf/current/GPG-KEY-EUGridPMA-RPM-3 | apt-key add -
 
 apt-get update -qq
-apt-get install -y ca-policy-egi-core fetch-crl apparmor ca-certificates iptables lxc-docker-1.6.2 curl
+apt-get install -y ca-policy-egi-core fetch-crl apparmor ca-certificates iptables docker-engine=1.9.1-0~trusty curl
 apt-get clean
 rm -rf /var/lib/apt/lists/*
 
