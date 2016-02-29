9318f8224b993d0ad8511038aaadef3dd0358359
enricovianello <maverick17586@gmail.com>
2016-02-29 17:49:46 +0100

    Added fs_type variable to storm-testsuite launch

--- storm-testsuite-image/setup_testsuite.sh
+++ storm-testsuite-image/setup_testsuite.sh
@@ -6,6 +6,7 @@ STORM_BE_HOST="${STORM_BE_HOST:-docker-storm.cnaf.infn.it}"
 STORM_BE_SYNC_PORT="${STORM_BE_SYNC_PORT:-8444}"
 TESTSUITE_BRANCH="${TESTSUITE_BRANCH:-develop}"
 TESTSUITE_SUITE="${TESTSUITE_SUITE:-tests}"
+FS_TYPE="${FS_TYPE:-ext4}"
 
 echo 'export X509_USER_PROXY="/tmp/x509up_u$(id -u)"'>/etc/profile.d/x509_user_proxy.sh
 
@@ -39,7 +40,7 @@ else
   EXCLUDE="--exclude $TESTSUITE_EXCLUDE" 
 fi
 
-pybot_cmd="pybot --pythonpath .:lib --variable backEndHost:$STORM_BE_HOST $EXCLUDE -d reports -s $TESTSUITE_SUITE tests"
+pybot_cmd="pybot --pythonpath .:lib --variable backEndHost:$STORM_BE_HOST $EXCLUDE --variable fsType:$FS_TYPE -d reports -s $TESTSUITE_SUITE tests"
 
 # install and execute the StoRM testsuite (develop version) as user "tester"
 exec su - tester sh -c "git clone $TESTSUITE; cd /home/tester/storm-testsuite; git checkout $TESTSUITE_BRANCH; $pybot_cmd"
