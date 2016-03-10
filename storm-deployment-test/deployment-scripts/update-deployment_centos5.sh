8449fecae09f69e7a28b69b83d5f77961263c555
enricovianello <maverick17586@gmail.com>
2016-03-10 15:07:52 +0100

    Fixed update deployment storm for SL5 and scripts names

--- storm-deployment-test/deployment-scripts/update-deployment_centos5.sh
+++ storm-deployment-test/deployment-scripts/update-deployment_centos5.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+fix_yaim () {
+  # avoid starting frontend server
+  sed -i -e '/\/sbin\/service storm-frontend-server start/c\\#\/sbin\/service storm-frontend-server start' /opt/glite/yaim/functions/local/config_storm_frontend
+
+  # avoid ntp check
+  echo "config_ntp () {"> /opt/glite/yaim/functions/local/config_ntp
+  echo "return 0">> /opt/glite/yaim/functions/local/config_ntp
+  echo "}">> /opt/glite/yaim/functions/local/config_ntp
+}
+
 # This script execute a clean deployment of StoRM
 WGET_OPTIONS="--no-check-certificate"
 
@@ -25,6 +35,12 @@ usermod -a -G storm gridhttps
 # install yaim configuration
 sh ./install-yaim-configuration.sh
 
+# Sleep more avoid issues on docker
+sed -i 's/sleep 20/sleep 30/' /etc/init.d/storm-backend-server
+
+# Sleep more in bdii init script to avoid issues on docker
+sed -i 's/sleep 2/sleep 5/' /etc/init.d/bdii
+
 # do yaim
 /opt/glite/yaim/bin/yaim -c -d 6 -s /etc/storm/siteinfo/storm.def -n se_storm_backend -n se_storm_frontend -n se_storm_gridftp -n se_storm_gridhttps
 
@@ -34,6 +50,16 @@ wget $WGET_OPTIONS  $STORM_REPO -O /etc/yum.repos.d/storm.repo
 # update
 yum clean all
 yum update -y
+
+if [ $? != 0 ]; then
+    echo "Problem occurred while updating the system!"
+    exit 1
+fi
+
+fix_yaim
+
+# Sleep more avoid issues on docker
+sed -i 's/sleep 20/sleep 30/' /etc/init.d/storm-backend-server
 
 # run post-installation config script
 sh ./post-config-setup.sh
