fdace0bfd91955753fb8dadbf4b1e805de233d07
enricovianello <maverick17586@gmail.com>
2016-03-10 16:46:13 +0100

    Increased bdii sleep time

--- storm-deployment-test/deployment-scripts/clean-deployment_centos5.sh
+++ storm-deployment-test/deployment-scripts/clean-deployment_centos5.sh
@@ -38,7 +38,7 @@ sh ./install-yaim-configuration.sh
 sed -i 's/sleep 20/sleep 30/' /etc/init.d/storm-backend-server
 
 # Sleep more in bdii init script to avoid issues on docker
-sed -i 's/sleep 2/sleep 5/' /etc/init.d/bdii
+sed -i 's/sleep 2/sleep 20/' /etc/init.d/bdii
 
 # configure with yaim
 /opt/glite/yaim/bin/yaim -c -s /etc/storm/siteinfo/storm.def -n se_storm_backend -n se_storm_frontend -n se_storm_gridftp -n se_storm_gridhttps
