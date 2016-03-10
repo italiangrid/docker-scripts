8f214f0b043c673723a606a5cf07486275ecaeff
enricovianello <maverick17586@gmail.com>
2016-03-10 14:23:36 +0100

    Restored pull false on building storm-deployment-image

--- storm-deployment-test/build-images.sh
+++ storm-deployment-test/build-images.sh
@@ -3,7 +3,7 @@ set -ex
 tags=${tags:-"centos5 centos6"}
 
 for t in ${tags}; do
-    docker build --pull=true \
+    docker build --pull=false \
       --rm=true --no-cache=true \
       -t italiangrid/storm-deployment-test:${t} -f Dockerfile.${t} .
 done
