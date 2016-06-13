9cce6139c7e6d4d03e0fab3ecf5150a87d00c561
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2016-06-13 12:08:40 +0200

    Fixed push-image script

--- jenkins-slave-centos7/push-image.sh
+++ jenkins-slave-centos7/push-image.sh
@@ -5,7 +5,7 @@ IMAGE_NAME="italiangrid/jenkins-slave-centos7"
 
 if [ -n "${DOCKER_REGISTRY_HOST}" ]; then
     docker tag -f  ${IMAGE_NAME} ${DOCKER_REGISTRY_HOST}/${IMAGE_NAME}
-    docker push ${IMAGE_NAME}
+    docker push ${DOCKER_REGISTRY_HOST}/${IMAGE_NAME}
 else
     echo "Please define the DOCKER_REGISTRY_HOST env variable"
     exit 1
