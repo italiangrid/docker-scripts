3d07457dc59de67e0ac582913d3f571c10e6add1
enricovianello <maverick17586@gmail.com>
2017-06-12 14:54:58 +0200

    Launch new deployment script for cdmi from deployment repo

--- cdmi-storm/launch_cdmi.sh
+++ cdmi-storm/launch_cdmi.sh
@@ -1,46 +1,18 @@
 #!/bin/bash
 set -x
 
-# install cdmi_storm
-wget $STORM_REPO -O /etc/yum.repos.d/cdmi-storm.repo
-yum clean all
-yum install -y cdmi-storm
+MODE="${MODE:-clean}"
+PLATFORM="${PLATFORM:-centos6}"
+STORM_DEPLOYMENT_TEST_BRANCH=${STORM_DEPLOYMENT_TEST_BRANCH:-master}
 
-# Configure
 if [ -z ${CLIENT_ID+x} ]; then echo "CLIENT_ID is unset"; exit 1; fi
 if [ -z ${CLIENT_SECRET+x} ]; then echo "CLIENT_SECRET is unset"; exit 1; fi
 
-STORM_DEPLOYMENT_TEST_BRANCH="${STORM_DEPLOYMENT_TEST_BRANCH:-master}"
-
 cd /
 git clone https://github.com/italiangrid/storm-deployment-test.git --branch $STORM_DEPLOYMENT_TEST_BRANCH
-cd /storm-deployment-test
+cd /storm-deployment-test/docker
 
-cp -rf cdmi/var/lib/cdmi-server/config/application.yml /var/lib/cdmi-server/config/application.yml
-cp -rf cdmi/etc/cdmi-server/plugins/capabilities /etc/cdmi-server/plugins/
+DEPLOYMENT_SCRIPT="$MODE-cdmi-deployment_$PLATFORM.sh"
 
-cd /var/lib/cdmi-server/config
-sed -i 's/CLIENT_ID/${CLIENT_ID}/g' application.yml
-sed -i 's/CLIENT_SECRET/${CLIENT_SECRET}/g' application.yml
-
-MAX_RETRIES=600
-attempts=1
-CMD="nc -w1 ${REDIS_HOSTNAME} 6379"
-
-echo "Waiting for Redis server ... "
-$CMD
-
-while [ $? -eq 1 ] && [ $attempts -le  $MAX_RETRIES ];
-do
-  sleep 5
-  let attempts=attempts+1
-  $CMD
-done
-
-if [ $attempts -gt $MAX_RETRIES ]; then
-    echo "Timeout!"
-    exit 1
-fi
-
-cd /var/lib/cdmi-server
-./cdmi-server-1.2.jar
+chmod +x $DEPLOYMENT_SCRIPT
+./$DEPLOYMENT_SCRIPT ${CLIENT_ID} ${CLIENT_SECRET}
