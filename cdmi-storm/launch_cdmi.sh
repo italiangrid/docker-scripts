68c584dd3f497bcd7d33694fa7bfee65dd7bcd45
enricovianello <maverick17586@gmail.com>
2017-06-13 09:55:25 +0200

    Used own cdmi-server instead of indigo

--- cdmi-storm/launch_cdmi.sh
+++ cdmi-storm/launch_cdmi.sh
@@ -4,6 +4,7 @@ set -x
 MODE="${MODE:-clean}"
 PLATFORM="${PLATFORM:-centos6}"
 STORM_DEPLOYMENT_TEST_BRANCH=${STORM_DEPLOYMENT_TEST_BRANCH:-master}
+REDIS_HOSTNAME=${REDIS_HOSTNAME:-redis.cnaf.infn.it}
 
 if [ -z ${CLIENT_ID+x} ]; then echo "CLIENT_ID is unset"; exit 1; fi
 if [ -z ${CLIENT_SECRET+x} ]; then echo "CLIENT_SECRET is unset"; exit 1; fi
@@ -15,4 +16,4 @@ cd /storm-deployment-test/docker
 DEPLOYMENT_SCRIPT="$MODE-cdmi-deployment_$PLATFORM.sh"
 
 chmod +x $DEPLOYMENT_SCRIPT
-./$DEPLOYMENT_SCRIPT $CLIENT_ID $CLIENT_SECRET
+./$DEPLOYMENT_SCRIPT $CLIENT_ID $CLIENT_SECRET $REDIS_HOSTNAME
