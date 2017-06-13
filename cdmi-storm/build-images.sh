8a9f048ac0689c65c780e5c6707cc9f8a4b351c5
enricovianello <maverick17586@gmail.com>
2017-06-13 16:24:39 +0200

    Protect secrets

--- cdmi-storm/build-images.sh
+++ cdmi-storm/build-images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 
-docker build --pull=false --rm=true -t italiangrid/cdmi-storm .
+docker build --pull=false --rm=true --build-arg CLIENT_ID=${CLIENT_ID} --build-arg CLIENT_SECRET=${CLIENT_SECRET} -t italiangrid/cdmi-storm .
--- cdmi-storm/launch_cdmi.sh
+++ cdmi-storm/launch_cdmi.sh
@@ -4,10 +4,30 @@ set -x
 MODE="${MODE:-clean}"
 PLATFORM="${PLATFORM:-centos6}"
 STORM_DEPLOYMENT_TEST_BRANCH=${STORM_DEPLOYMENT_TEST_BRANCH:-master}
-REDIS_HOSTNAME=${REDIS_HOSTNAME:-redis.cnaf.infn.it}
+REDIS_HOSTNAME="${REDIS_HOSTNAME:-localhost}"
 
 if [ -z ${CLIENT_ID+x} ]; then echo "CLIENT_ID is unset"; exit 1; fi
 if [ -z ${CLIENT_SECRET+x} ]; then echo "CLIENT_SECRET is unset"; exit 1; fi
+
+# Wait for redis server
+MAX_RETRIES=600
+attempts=1
+CMD="nc -w1 ${REDIS_HOSTNAME} 6379"
+
+echo "Waiting for Redis server ... "
+$CMD
+
+while [ $? -eq 1 ] && [ $attempts -le  $MAX_RETRIES ];
+do
+  sleep 5
+  let attempts=attempts+1
+  $CMD
+done
+
+if [ $attempts -gt $MAX_RETRIES ]; then
+    echo "Timeout!"
+    exit 1
+fi
 
 cd /
 git clone https://github.com/italiangrid/storm-deployment-test.git --branch $STORM_DEPLOYMENT_TEST_BRANCH
@@ -16,4 +36,4 @@ cd /storm-deployment-test/docker
 DEPLOYMENT_SCRIPT="$MODE-cdmi-deployment_$PLATFORM.sh"
 
 chmod +x $DEPLOYMENT_SCRIPT
-./$DEPLOYMENT_SCRIPT $CLIENT_ID $CLIENT_SECRET $REDIS_HOSTNAME
+./$DEPLOYMENT_SCRIPT
