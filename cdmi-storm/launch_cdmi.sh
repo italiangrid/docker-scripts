7c1575467367c43369fdc26105bf456152a8bf7e
enricovianello <maverick17586@gmail.com>
2017-06-14 17:30:35 +0200

    Default value for CDMI_CLIENT_ID

--- cdmi-storm/launch_cdmi.sh
+++ cdmi-storm/launch_cdmi.sh
@@ -5,8 +5,7 @@ MODE="${MODE:-clean}"
 PLATFORM="${PLATFORM:-centos6}"
 STORM_DEPLOYMENT_TEST_BRANCH=${STORM_DEPLOYMENT_TEST_BRANCH:-master}
 REDIS_HOSTNAME="${REDIS_HOSTNAME:-localhost}"
-
-if [ -z ${CDMI_CLIENT_ID+x} ]; then echo "CDMI_CLIENT_ID is unset"; exit 1; fi
+CDMI_CLIENT_ID="${CDMI_CLIENT_ID:-838129a5-84ca-4dc4-bfd8-421ee317aabd}"
 if [ -z ${CDMI_CLIENT_SECRET+x} ]; then echo "CDMI_CLIENT_SECRET is unset"; exit 1; fi
 
 # Wait for redis server
