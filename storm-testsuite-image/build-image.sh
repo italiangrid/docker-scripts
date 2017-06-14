c08af37d1343732d7b9a3fca5812250959f92f27
enricovianello <maverick17586@gmail.com>
2017-06-14 16:52:33 +0200

    Removed no more used build args

--- storm-testsuite-image/build-image.sh
+++ storm-testsuite-image/build-image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build --build-arg CDMI_CLIENT_SECRET=${CDMI_CLIENT_SECRET} --build-arg IAM_USER_PASSWORD=${IAM_USER_PASSWORD} -t italiangrid/storm-testsuite .
+docker build -t italiangrid/storm-testsuite .
