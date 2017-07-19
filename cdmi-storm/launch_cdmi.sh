b048c008be07523b8c3148a838723ab22c52bf76
enricovianello <maverick17586@gmail.com>
2017-07-19 18:41:23 +0200

    Removed wait for redis server on cdmi-storm launch

--- cdmi-storm/launch_cdmi.sh
+++ cdmi-storm/launch_cdmi.sh
@@ -8,26 +8,6 @@ REDIS_HOSTNAME="${REDIS_HOSTNAME:-localhost}"
 CDMI_CLIENT_ID="${CDMI_CLIENT_ID:-838129a5-84ca-4dc4-bfd8-421ee317aabd}"
 if [ -z ${CDMI_CLIENT_SECRET+x} ]; then echo "CDMI_CLIENT_SECRET is unset"; exit 1; fi
 
-# Wait for redis server
-MAX_RETRIES=600
-attempts=1
-CMD="nc -w1 ${REDIS_HOSTNAME} 6379"
-
-echo "Waiting for Redis server ... "
-$CMD
-
-while [ $? -eq 1 ] && [ $attempts -le  $MAX_RETRIES ];
-do
-  sleep 5
-  let attempts=attempts+1
-  $CMD
-done
-
-if [ $attempts -gt $MAX_RETRIES ]; then
-    echo "Timeout!"
-    exit 1
-fi
-
 cd /
 git clone https://github.com/italiangrid/storm-deployment-test.git --branch $STORM_DEPLOYMENT_TEST_BRANCH
 cd /storm-deployment-test/docker
