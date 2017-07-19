85c3ad24da5d5e0491c31a0bc915cc19b8a2af92
enricovianello <maverick17586@gmail.com>
2017-07-19 15:24:53 +0200

    removed -f from docker tag

--- cdmi-storm/push-images.sh
+++ cdmi-storm/push-images.sh
@@ -2,10 +2,6 @@
 set -ex
 
 if [ -n "${DOCKER_REGISTRY_HOST}" ]; then
-  docker tag -f  italiangrid/cdmi-storm ${DOCKER_REGISTRY_HOST}/italiangrid/cdmi-storm
+  docker tag italiangrid/cdmi-storm ${DOCKER_REGISTRY_HOST}/italiangrid/cdmi-storm
   docker push ${DOCKER_REGISTRY_HOST}/italiangrid/cdmi-storm
-fi
-
-if [ -n "${PUSH_TO_DOCKERHUB}" ]; then
-  docker push italiangrid/cdmi-storm
 fi
--- storm-deployment-test/push-images.sh
+++ storm-deployment-test/push-images.sh
@@ -4,13 +4,7 @@ tags=${tags:-"centos6"}
 
 if [ -n "${DOCKER_REGISTRY_HOST}" ]; then
   for t in ${tags}; do
-    docker tag -f  italiangrid/storm-deployment-test:${t} ${DOCKER_REGISTRY_HOST}/italiangrid/storm-deployment-test:${t}
+    docker tag italiangrid/storm-deployment-test:${t} ${DOCKER_REGISTRY_HOST}/italiangrid/storm-deployment-test:${t}
     docker push ${DOCKER_REGISTRY_HOST}/italiangrid/storm-deployment-test:${t}
-  done
-fi
-
-if [ -n "${PUSH_TO_DOCKERHUB}" ]; then
-  for t in ${tags}; do
-    docker push italiangrid/storm-deployment-test:${t}
   done
 fi
