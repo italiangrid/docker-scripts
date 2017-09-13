5438c337c2380e135077111fb7e346daac4e7af5
enricovianello <maverick17586@gmail.com>
2017-09-13 16:49:08 +0200

    Little adjustments to storm-deployment-image deploy script

--- storm-deployment-test/deploy.sh
+++ storm-deployment-test/deploy.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 set -x
 
-MODE="${MODE:-clean}"
-PLATFORM="${PLATFORM:-centos6}"
-STORM_DEPLOYMENT_TEST_BRANCH=${STORM_DEPLOYMENT_TEST_BRANCH:-master}
+MODE="${MODE:-"clean"}"
+PLATFORM="${PLATFORM:-"centos6"}"
+DEPLOYMENT_SCRIPT="$MODE-deployment_$PLATFORM.sh"
 
-# install host certificate
-cp /storm-certificates/docker_storm_cnaf_infn_it.cert.pem /etc/grid-security/hostcert.pem
-cp /storm-certificates/docker_storm_cnaf_infn_it.key.pem /etc/grid-security/hostkey.pem
-chmod 400 /etc/grid-security/hostkey.pem
-chmod 644 /etc/grid-security/hostcert.pem
-
-# setup StoRM services
-service rsyslog start
+STORM_DEPLOYMENT_TEST_BRANCH=${STORM_DEPLOYMENT_TEST_BRANCH:-"master"}
 
 cd /
 git clone https://github.com/italiangrid/storm-deployment-test.git --branch $STORM_DEPLOYMENT_TEST_BRANCH
 cd /storm-deployment-test/docker
-
-DEPLOYMENT_SCRIPT="$MODE-deployment_$PLATFORM.sh"
 
 chmod +x $DEPLOYMENT_SCRIPT
 ./$DEPLOYMENT_SCRIPT
--- storm-deployment-test/setup.sh
+++ storm-deployment-test/setup.sh
@@ -31,3 +31,11 @@ fetch-crl
 if [ $? != 0 ]; then
   exit 1
 fi
+
+# install host certificate
+cp /storm-certificates/docker_storm_cnaf_infn_it.cert.pem /etc/grid-security/hostcert.pem
+cp /storm-certificates/docker_storm_cnaf_infn_it.key.pem /etc/grid-security/hostkey.pem
+chmod 400 /etc/grid-security/hostkey.pem
+chmod 644 /etc/grid-security/hostcert.pem
+
+service rsyslog start
